d9e8f34795f8053b1fe8fa363bfa9e33f36ac5a4
house <czerwinskidomi@gmail.com>
2017-11-29 22:35:34 +0100

    Dodanie list i metod obslugi

--- src/libraryOfKnowledge/Library.java
+++ src/libraryOfKnowledge/Library.java
@@ -6,22 +6,42 @@ import java.util.List;
 public class Library {
 	private String name;
 	private String author;
+	private String together;
 	
-	private List<String> ksiazki = new ArrayList<String>();
-	private List<String> filmy = new ArrayList<String>();
-	
-	Library() {
-		
-	}
+	private List<String> books = new ArrayList<>();
+	private List<String> movies = new ArrayList<>();
 	
 	Library(String n, String a) {
 		name = n;
 		author = a;
 	}
 	
-	String getName() { return name; }
-	String getAuthor() { return author; }
-	void setName(String n) { name = n; }
-	void setAuthor(String a) { author = a; }
+	Library(String t){
+		t = name + " " + author;
+		together = t;
+	}
+	
+	void setName(String n) {
+		name = n;
+	}
+	void setAuthor(String a) {
+		author = a;
+	}	
+	void addBook(String t) {
+		t = together;
+		books.add(t);
+	}
+	void addMovie(String t) {
+		t = together;
+		movies.add(t);
+	}
+	void getBooks(String t) {
+		t = together;
+		books.remove(t);
+	}
+	void getMovies(String t) {
+		t = together;
+		movies.remove(t);
+	}
 
 }
