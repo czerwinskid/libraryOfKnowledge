2081b0e87630c284044b35e567d2df2c1051c2df
house <czerwinskidomi@gmail.com>
2017-11-29 22:11:12 +0100

    Dodanie list i metod obslugi

--- src/libraryOfKnowledge/Library.java
+++ src/libraryOfKnowledge/Library.java
@@ -1,5 +1,27 @@
 package libraryOfKnowledge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Library {
+	private String name;
+	private String author;
+	
+	private List<String> ksiazki = new ArrayList<String>();
+	private List<String> filmy = new ArrayList<String>();
+	
+	Library() {
+		
+	}
+	
+	Library(String n, String a) {
+		name = n;
+		author = a;
+	}
+	
+	String getName() { return name; }
+	String getAuthor() { return author; }
+	void setName(String n) { name = n; }
+	void setAuthor(String a) { author = a; }
 
 }
