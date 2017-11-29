f6b948dec111f679da8ec5d5a424a53e06e25e97
house <czerwinskidomi@gmail.com>
2017-11-29 23:34:58 +0100

    Po usunieciu i przepisaniu kodu

--- src/libraryOfKnowledge/Librarian.java
+++ src/libraryOfKnowledge/Librarian.java
@@ -1,10 +1,29 @@
 package libraryOfKnowledge;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
 public class Librarian extends Library{
 	public static void main(String args[]) {
-		Library l1 = new Library("LOTR", "Tolkien");
-	
-		l1.showBooks("LOTR Tolkien");
-	
+		
+		List<String> books = new ArrayList<>();
+		Library l1 = new Library("Lord of The Ring", "Tolkien");
+		Library l2 = new Library("Harry Potter", "J.K. Rowling");
+				
+		books.add(l1.getBook());
+		books.add(l2.getBook());
+
+		Iterator show = books.iterator();
+		int i = 0;
+		
+		while (show.hasNext()) {
+			System.out.println(show.next());
+		}
+		
+		
+		
+		
 	}
+	
 }
--- src/libraryOfKnowledge/Library.java
+++ src/libraryOfKnowledge/Library.java
@@ -1,56 +1,29 @@
 package libraryOfKnowledge;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Library {
 	private String name;
 	private String author;
 	private String together;
 	
-	private List<String> books = new ArrayList<>();
-	private List<String> movies = new ArrayList<>();
-	
-	public Library(){
-		
+	public Library() {
+		name = null;
+		author = null;
 	}
+	
 	public Library(String n, String a) {
 		name = n;
 		author = a;
+		
 	}
 	
-	public Library(String t){
-		t = name + " " + author;
-		together = t;
-	}
-	
-	public void setName(String n) {
+	void setName(String n) {
 		name = n;
 	}
-	public void setAuthor(String a) {
+	void setAuthor(String a) {
 		author = a;
-	}	
-	public void addBook(String t) {
-		t = together;
-		books.add(t);
 	}
-	public void addMovie(String t) {
-		t = together;
-		movies.add(t);
-	}
-	public void deleteBooks(String t) {
-		t = together;
-		books.remove(t);
-	}
-	public void deleteMovies(String t) {
-		t = together;
-		movies.remove(t);
-	}
-	
-	public void showBooks(String book) {
-		book = together;
-		int s = books.indexOf(book);
-		System.out.println(s);
+	String getBook() {
+		return "Nazwa: " + name + ", Autor: " + author;
 	}
 
 }
