c8133a24e7cae7232997f2c50c95ed59fe5fa724
house <czerwinskidomi@gmail.com>
2017-11-30 01:08:43 +0100

    wypozyczalnia gotowa

--- src/libraryOfKnowledge/Client.java
+++ src/libraryOfKnowledge/Client.java
@@ -1,5 +1,7 @@
 package libraryOfKnowledge;
 
+import java.util.Iterator;
+
 public class Client extends Librarian {
 	
 	public static void main(String args[]) {
@@ -7,9 +9,31 @@ public class Client extends Librarian {
 		addToLibrary();
 		
 		String x = c1.getBook();
-		System.out.println(x);
+		
 		
 		borrowBook(x);
+		
+		System.out.println("[BIBLIOTEKA]");
+		System.out.println("Ksi¹¿ki w naszej bibliotece:");
+		Iterator<String> showBooks = books.iterator();
+		
+		while (showBooks.hasNext()) {
+			System.out.print("  ");
+			System.out.println(showBooks.next());			
+		}System.out.println();		
+		
+		Iterator<String> showMovies = movies.iterator();
+		
+		System.out.println("Filmy w naszej bibliotece:");
+		
+		while (showMovies.hasNext()) {
+			System.out.print("  ");
+			System.out.println(showMovies.next());
+		}System.out.println("[BIBLIOTEKA]");
+		System.out.println();		
+		
+		returnBook(books.get(0));
+		System.out.println(books.get(0));
 	}
 
 }
--- src/libraryOfKnowledge/Librarian.java
+++ src/libraryOfKnowledge/Librarian.java
@@ -17,60 +17,58 @@ public class Librarian extends Library{
 	static List<String> books = new ArrayList<>();
 	static List<String> movies = new ArrayList<>();
 	
-		public static void addToLibrary() {
-			Library b1 = new Library("Lord of The Ring", "Tolkien");
-			Library b2 = new Library("Harry Potter", "J.K. Rowling");
+	public static void addToLibrary() {
+		Library b1 = new Library("Lord of The Ring", "Tolkien");
+		Library b2 = new Library("Harry Potter", "J.K. Rowling");
 				
-			books.add(b1.getBook());
-			books.add(b2.getBook());
+		books.add(b1.getBook());
+		books.add(b2.getBook());
 		
-			Iterator<String> showBooks = books.iterator();
+		Iterator<String> showBooks = books.iterator();
+		System.out.println("[BIBLIOTEKA]");
+		System.out.println("Ksi¹¿ki w naszej bibliotece:");
 		
-			System.out.println("Ksi¹¿ki w naszej bibliotece:");
-		
-			while (showBooks.hasNext()) {
-				System.out.print("  ");
-				System.out.println(showBooks.next());			
-			}System.out.println();
+		while (showBooks.hasNext()) {
+			System.out.print("  ");
+			System.out.println(showBooks.next());			
+		}System.out.println();
 
 		
-			Library m1 = new Library("Lord of The Rings", "Peter Jackson");
-			Library m2 = new Library("Harry Potter: Kamieñ", "Chris Columbus");
+		Library m1 = new Library("Lord of The Rings", "Peter Jackson");
+		Library m2 = new Library("Harry Potter: Kamieñ", "Chris Columbus");
 				
-			movies.add(m1.getBook());
-			movies.add(m2.getBook());
+		movies.add(m1.getBook());
+		movies.add(m2.getBook());
 
-			Iterator<String> showMovies = movies.iterator();
+		Iterator<String> showMovies = movies.iterator();
 		
-			System.out.println("Filmy w naszej bibliotece:");
+		System.out.println("Filmy w naszej bibliotece:");
 		
-			while (showMovies.hasNext()) {
-				System.out.print("  ");
-				System.out.println(showMovies.next());
-			}System.out.println();				
-		}
+		while (showMovies.hasNext()) {
+			System.out.print("  ");
+			System.out.println(showMovies.next());
+		}System.out.println("[BIBLIOTEKA]");
+		System.out.println();				
+	}
 		
-		public static void borrowBook(String name) {
-			int k = books.indexOf(name);
-			books.set(k, "Wypo¿yczono: " + name);
-			
-			Iterator<String> showBooks = books.iterator();
-			while (showBooks.hasNext()) {
-				System.out.print("  ");
-				System.out.println(showBooks.next());			
-			}System.out.println();
-		}
-		public void returnBook(String name) {
-			int k = books.indexOf(name);
-			books.set(k, name);
-		}
-		public void borrowMovie(String name) {
-			int k = books.indexOf(name);
-			movies.set(k, "Wypo¿yczono: " + name);
-		}
-		public void returnMovie(String name) {
-			int k = books.indexOf(name);
-			movies.set(k, name);
-		}
+	public static void borrowBook(String name) {
+		int k = books.indexOf(name);
+		books.set(k, "Wypo¿yczono: " + name);		
+	}
+	public static void returnBook(String name) {
+		String cutedname = name.replaceAll("(.*)Wypo¿yczono: ","");
+		int k = books.indexOf(name);
+		books.set(k, cutedname);		
+	}
+	public static void borrowMovie(String name) {
+		int k = movies.indexOf(name);
+		movies.set(k, "Wypo¿yczono: " + name);
+		
+	}
+	public static void returnMovie(String name) {		
+		String cutedname = name.replaceAll("(.*)Wypo¿yczono: ","");
+		int k = movies.indexOf(name);
+		movies.set(k, cutedname);
+	}
 	
 }
